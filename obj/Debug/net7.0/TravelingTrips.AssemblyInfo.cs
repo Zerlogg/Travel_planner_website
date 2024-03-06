8f999a9e17e3c835205693e8b9a70d87a11ad836
Emils Strelis <emils.strelis@globalsoftware.ai>
2024-03-06 16:35:34 +0200

    Additional files

--- obj/Debug/net7.0/TravelingTrips.AssemblyInfo.cs
+++ obj/Debug/net7.0/TravelingTrips.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TravelingTrips")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61ac5a53efe80e9a9d3c2d674b1b4dcbf917e0ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("TravelingTrips")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TravelingTrips")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
