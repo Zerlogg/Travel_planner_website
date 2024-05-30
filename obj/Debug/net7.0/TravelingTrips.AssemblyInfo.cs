b2a8ef170e43858ac0f8583a60230265572b0777
Emils Strelis <emils.strelis@globalsoftware.ai>
2024-05-30 14:51:00 +0300

    Small travel page design changes

--- obj/Debug/net7.0/TravelingTrips.AssemblyInfo.cs
+++ obj/Debug/net7.0/TravelingTrips.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TravelingTrips")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f50605e7d511a350d723ae43c740f82833416971")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a1faf658d0817f1bbfbedee01401e9c141a182e")]
 [assembly: System.Reflection.AssemblyProductAttribute("TravelingTrips")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TravelingTrips")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
